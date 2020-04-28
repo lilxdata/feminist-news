f2e5e5e7964d7ceef600be0e36e5f598f3789338
lilxdata <madeline_pignetti@hmc.edu>
2020-04-28 11:02:37 -0700

    Initial Commit

---
+++ news/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  news
+//
+//  Created by madi on 4/28/20.
+//  Copyright © 2020 madi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
